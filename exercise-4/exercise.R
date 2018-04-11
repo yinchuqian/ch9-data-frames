36affbea61125feae7d164f0ecde249b52dcb1cf
Chuqian Yin <yinchuqian@yinchuqiandeMacBook-Air.local>
2018-04-10 22:38:01 -0700

    practice more about data frames(add, modify,and retrive datas )

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -4,36 +4,36 @@
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-
+gates_money <- read.csv("data/gates_money.csv", stringsAsFactors = FALSE)
 
 # Use the View function to look at the loaded data
-
+View(gates_money)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
-
+organization <- gates_money$organization
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
 # This is a useful debugging tip if you hit errors later!
-
+is.vector(organization)
 
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
-
+mean_value <- mean(gates_money$total_amount)
 
 # What was the dollar amount of the largest grant?
-
+largest_grant <- max(gates_money$total_amount)
 
 # What was the dollar amount of the smallest grant?
-
+smallest_grant <- min(gates_money$total_amount)
 
 # Which organization received the largest grant?
-
+largest_organization <- gates_money$organization[gates_money$total_amount == largest_grant]
 
 # Which organization received the smallest grant?
-
+smallest_organization <- gates_money$organization[gates_money$total_amount == smallest_grant]
 
 # How many grants were awarded in 2010?
-
+length(gates_money$total_amount[gates_money$start_year == 2010])
--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -2,22 +2,25 @@
 
 # Read in the female baby names data file found in the `data` folder into a 
 # variable called `names`. Remember to NOT treat the strings as factors!
-
+female_names <- read.csv("data/female_names.csv", stringsAsFactors = FALSE)
 
 # Create a data frame `names_2013` that contains only the rows for the year 2013
-
-
+names_2013 <- female_names[female_names$year == 2013, ]
 # What was the most popular female name in 2013?
-
+popular <- names_2013[names_2013$prop == max(names_2013$prop), 'name']
 
 # Write a function `most_popular_in_year` that takes in a year as a value and 
 # returns the most popular name in that year
-
+most_popular_in_year <- function(year_value){
+  names_year <- female_names[female_names$year == year_value, ]
+  popular <- names_year[names_year$prop == max(names_year$prop), 'name']
+  return(popular)
+}
 
 # What was the most popular female name in 1994?
+name_in_2014 <- most_popular_in_year(1994)
 
-
-# Write a function `number_in_million` that takes in a name and a year, and 
+  # Write a function `number_in_million` that takes in a name and a year, and 
 # returns statistically how many babies out of 1 million born that year have 
 # that name. 
 # Hint: get the popularity percentage, and take that percentage out of 1 million.
